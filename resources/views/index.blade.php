6c2c8723d36c7e4b2d66a0cb87eaefdc9bf02373
azevedo2elve <azevedogabriel00@gmail.com>
2024-05-23 01:53:22 -0300

    adding background image and testing arrays in the frontend

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,11 +9,17 @@
 </head>
 <body>
     <?php
-        echo $sheets[0][8];
+        foreach ($sheets as $key => $value) {
+            //echo $value[0] . ' ------------------------- ' . $value[8] . '<br>';
+        }
     ?>
 
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    <div class="container mx-auto">
+        <div id="cardRS" class="flex justify-center border border-blue-700 h-[1920px] w-[1080px]">
+            <div class="border border-red-600 mt-96 mx-48 text-5xl text-white">
+                <?php echo $sheets[1][0] . '<br>' . '<br>' . $sheets[1][8] . '<br>' . '<br>' . 'Endereço:' . '<br>' . $sheets[1][1] . ' - ' . $sheets[1][2]; ?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
